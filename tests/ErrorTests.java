a77311648a20e2ccd8222fde26217fb05df03665
Ramona Botezatu <ramonabotezatu@Ramonas-MacBook-Pro.local>
2020-01-18 20:33:26 +0200

    Refactoring tests

--- tests/ErrorTests.java
+++ tests/ErrorTests.java
@@ -1,9 +1,2 @@
-import org.junit.experimental.categories.Categories;
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-
-@RunWith(Categories.class)
-@Categories.IncludeCategory(ErrorTests.class)
-@Suite.SuiteClasses(FoodTest.class)
 public class ErrorTests {
 }
